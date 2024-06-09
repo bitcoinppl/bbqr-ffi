37eed0d4f4bf7fc2e3d655c760947dc97a1d8ab1
Praveen Perera <praveen@avencera.com>
2024-06-08 21:06:14 -0500

    Add example iOS app

---
+++ examples/ios/QrDemo/QrDemo/QrDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrDemoApp.swift
+//  QrDemo
+//
+//  Created by Praveen Perera on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct QrDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
