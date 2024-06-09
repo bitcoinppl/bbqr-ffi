f223fc2e6fde7446ca0a182410f19ac721d5f4d1
Praveen Perera <praveen@avencera.com>
2024-06-09 10:00:04 -0500

    Fix Package.swift

--- bbqr-swift/Package.swift
+++ bbqr-swift/Package.swift
@@ -1,26 +1,24 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "BbqrSwift",
-    platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
-    ],
+    name: "Bbqr",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Bbqr",
-            targets: ["bbqrFFI"]),
+            targets: ["Bbqr", "bbqrFFI"]
+        ),
     ],
-    dependencies: [],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "bbqrFFI", path: "./bbqrFFI.xcframework"),
         .target(
             name: "Bbqr",
             dependencies: ["bbqrFFI"]
         ),
-
     ]
 )
