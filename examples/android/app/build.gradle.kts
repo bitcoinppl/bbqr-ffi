fb1eda1910ddb7980d0d350cf8437ccd3a46a8f6
Praveen Perera <me@praveenperera.com>
2026-05-26 11:28:27 -0500

    Bump Android docs to 0.4.1
    Update Android integration examples and publishing version metadata to reference bbqr-android 0.4.1

--- examples/android/app/build.gradle.kts
+++ examples/android/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("org.bitcoinppl:bbqr-android:0.3.1")
+    implementation("org.bitcoinppl:bbqr-android:0.4.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
